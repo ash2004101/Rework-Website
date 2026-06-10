1d6be13af8e02870bfd7aa7fc7e55bd0c3fdcc4d
ash2004101 <soundararajanselvan2@gmail.com>
2026-06-10 20:24:20 +0530

    Added ScrollToTop component to fix navigation scroll bug

--- reworkiq-app/src/App.tsx
+++ reworkiq-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
@@ -9,6 +10,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
